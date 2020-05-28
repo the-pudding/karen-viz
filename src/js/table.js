fe0f9fcde75c7b8a03b5e4db1b5672f13f141280
Amber Thomas <ProQuestionAsker@gmail.com>
2020-05-27 18:46:32 -0700

    almost have dropdown working

--- src/js/table.js
+++ src/js/table.js
@@ -1,15 +1,18 @@
+// for typing name
+import Autocomplete from 'accessible-autocomplete';
+
 let data = null;
-const decFormat = d3.format('.2f');
 
 // selections
 const $section = d3.select('.compare');
 const $containers = $section.selectAll('.table');
 const $tables = $containers.selectAll('table');
+const $input = $section.select('#autocomplete');
 
 const COLUMNS = [
-  { title: 'In 10 Yrs', prop: '0' },
-  { title: 'In 20 Yrs', prop: '1' },
-  { title: 'In 30 Yrs', prop: '2' },
+  { title: '10 Years', prop: '0' },
+  { title: '20 Years', prop: '1' },
+  { title: '30 Years', prop: '2' },
 ];
 
 function setupTableHeader() {
@@ -25,7 +28,7 @@ function setupTableHeader() {
     table.append('tbody');
   });
 
-  setupTableBody();
+  setupTableBody('');
 }
 
 function setupData(gender) {
@@ -34,24 +37,24 @@ function setupData(gender) {
   const ten = filtered
     .sort((a, b) => d3.descending(a.corKaren10, b.corKaren10))
     .slice(0, 10)
-    .map((d) => `<span class='num'>${decFormat(d.corKaren10)}</span> ${d.name}`);
+    .map((d) => `<span class='num'>${d.corKaren10}</span> ${d.name}`);
 
   const twenty = filtered
     .sort((a, b) => d3.descending(a.corKaren20, b.corKaren20))
     .slice(0, 10)
-    .map((d) => `<span class='num'>${decFormat(d.corKaren20)}</span> ${d.name}`);
+    .map((d) => `<span class='num'>${d.corKaren20}</span> ${d.name}`);
 
   const thirty = filtered
     .sort((a, b) => d3.descending(a.corKaren30, b.corKaren30))
     .slice(0, 10)
-    .map((d) => `<span class='num'>${decFormat(d.corKaren30)}</span> ${d.name}`);
+    .map((d) => `<span class='num'>${d.corKaren30}</span> ${d.name}`);
 
   const zipped = d3.zip(ten, twenty, thirty);
 
   return zipped;
 }
 
-function setupTableBody() {
+function setupTableBody(userInput) {
   const getRowData = (d, i) => {
     return COLUMNS.map((c) => ({ value: d[c.prop], title: c.title }));
   };
@@ -60,7 +63,12 @@ function setupTableBody() {
     const table = d3.select(this);
     const chartGender = table.attr('data-gender');
 
-    const tableData = setupData(chartGender);
+    let tableData = null;
+
+    if (userInput.length > 0) {
+      const userCorr = data.filter((d) => d.name === userInput);
+      console.log(userCorr);
+    } else tableData = setupData(chartGender);
 
     const row = table
       .select('tbody')
@@ -70,9 +78,27 @@ function setupTableBody() {
 
     row
       .selectAll('td')
-      .data(getRowData, (d) => `${d.value}-${d.title}`)
-      .join((enter) => enter.append('td').attr('class', (d) => `${d.title}`))
-      .html((d) => d.value);
+      .data(getRowData, (e) => `${e.value}-${e.title}`)
+      .join((enter) => enter.append('td').attr('class', (e) => `${e.title}`))
+      .html((e) => e.value);
+  });
+}
+
+function setupTypeFunctionality() {
+  const onlyNames = data.map((d) => d.name);
+
+  console.log({ $input });
+  // const onlyNames = ['Amber', 'Parker', 'Pixel'];
+  Autocomplete({
+    element: document.querySelector('#autocomplete'),
+    id: 'my-autocomplete',
+    source: onlyNames,
+    displayMenu: 'overlay',
+    confirmOnBlur: true,
+    onConfirm(name) {
+      console.log({ name });
+      // setupTableBody(name);
+    },
   });
 }
 
@@ -81,6 +107,7 @@ function resize() {}
 function init(res) {
   data = res;
   setupTableHeader();
+  setupTypeFunctionality();
   console.log({ data });
 }
 
