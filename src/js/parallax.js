2ad831f9f2ab8469fb6d47f5b79c8228ba2cfb4e
Jan Diehm <jadiehm@gmail.com>
2020-05-29 16:57:34 -0500

    design

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,11 +1,14 @@
-const layer1Scale = d3.scaleLinear().domain([0,1]).range([0,-60])
-const layer2Scale = d3.scaleLinear().domain([0,1]).range([0,-10])
-const layer3Scale = d3.scaleLinear().domain([0,1]).range([0,-20])
+const layer1Scale = d3.scaleLinear().domain([0,1]).range([0,-20])
+const layer2Scale = d3.scaleLinear().domain([0,1]).range([0,-2])
+const layer3Scale = d3.scaleLinear().domain([0,1]).range([0,-5])
 
 const $introImage = d3.select('.intro__image')
 const $layer1 = d3.select('.layer1')
 const $layer2 = d3.select('.layer2')
 const $layer3 = d3.select('.layer3')
+const $layer1Img = $layer1.select('img')
+const $layer2Img = $layer3.select('img')
+const $layer3Img = $layer3.select('img')
 
 
 function parallaxImage() {
@@ -15,33 +18,25 @@ function parallaxImage() {
         let innerH = window.innerHeight
         let progress = Math.round((scrollT / scrollH) * 100)
 
+        let layer1Top = $layer1.style('top').replace('px', '')
+        let layer2Top = $layer2.style('top')
+        let layer3Top = $layer3.style('top')
+
+       layer1Top = parseInt(layer1Top)
+       layer2Top = parseInt(layer2Top)
+       layer3Top = parseInt(layer3Top)
+
+
         if (progress <= 7) {
-            $layer1.style('top', `${layer1Scale(progress)}px`)
-            $layer2.style('top', `${layer2Scale(progress)}px`)
-            $layer3.style('top', `${layer3Scale(progress)}px`)
+            $layer1.style('top', `${70 + layer1Scale(progress)}%`)
+            $layer2.style('top', `${12 + layer2Scale(progress)}%`)
+            $layer3.style('top', `${15 + layer3Scale(progress)}%`)
         }
     })
 }
 
-function shiftImage() {
-    $layer1.classed('shifted', true)
-    $layer2.classed('shifted', true)
-    $layer3.classed('shifted', true)
-}
-
-function unshiftImage() {
-    $layer1.classed('shifted', false)
-    $layer2.classed('shifted', false)
-    $layer3.classed('shifted', false)
-}
-
-
-
-
 function init() {
-    //parallaxImage()
-    $introImage.on('mouseover', shiftImage)
-    $introImage.on('mouseleave', unshiftImage)  
+    parallaxImage()
 }
 
 export default { init };
